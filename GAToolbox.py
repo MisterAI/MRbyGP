88d8fffe5824b5f035ea20370a8eeddd574f7620
AndreasP <a15237@freenet.de>
2018-08-11 20:33:36 +0900

    Add twosided (lhs,rhs) mate and mutation
    (cherry picked from commit 5e70020ed613833e35c61f324efc37534d7feeda)

--- GAToolbox.py
+++ GAToolbox.py
@@ -4,10 +4,40 @@ import operator
 from deap import creator, base, tools, gp
 from FitnessEvaluation import get_fitness
 from FilterSet import filters
-from helperFunctions import protectedDiv, sinX, protectedPow, extendedCxOnePoint, extendedMutUniform, extendedSetStaticLimit
+from helperFunctions import protectedDiv, sinX, protectedPow
+
+def twosided_mate(ind1, ind2):
+	# Mate function for individuals consisting of two trees.
+	# It will mate both the left and the right hand-sides with each other
+	lhsInd1, rhsInd1 = ind1[0], ind1[1]
+	lhsInd2, rhsInd2 = ind2[0], ind2[1]
+	# In current state, simply crossover lhs with lhs and rhs with rhs
+	ind1[0], ind1[1] = gp.cxOnePoint(lhsInd1, lhsInd2)
+	ind2[0], ind2[1] = gp.cxOnePoint(rhsInd1, rhsInd2)
+
+	return ind1, ind2
+
+def twosided_mutate(individual, expr, pset):
+	# Mutation function for individuals consisting of two trees.
+	# It will randomly mutate either the first or the second tree
+	# of the individual
+	ind_fitness = individual.fitness
+	if random.random() < 0.5:
+		ind_tuple = gp.mutUniform(individual[0], expr, pset)
+		individual = creator.Individual((ind_tuple[0], individual[1]))
+	else:
+		ind_tuple = gp.mutUniform(individual[1], expr, pset)
+		individual = creator.Individual((individual[0], ind_tuple[0]))
+	individual.fitness = ind_fitness
+	return individual,
+
+def get_max_height(individual):
+	return max(individual[0].height, individual[1].height)
+
+def get_max_len(individual):
+	return max(len(individual[0]), len(individual[1]))
 
 def get_toolbox(target_func, weights):
-
 	# collect the atomic building blocks of the individuals
 	pset = gp.PrimitiveSet("MAIN", 1)
 	pset.addPrimitive(operator.add, 2)
@@ -27,41 +57,34 @@ def get_toolbox(target_func, weights):
 
 
 	# define the general form of an individual
+	# (one individual consists of two hand-sides, left and right;
+	# each hand-side consists of one computational graph)
 	creator.create("FitnessMulti", base.Fitness, weights=weights)
-	#creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMulti, target_func=target_func)
-	creator.create("Tree", gp.PrimitiveTree, target_func=target_func)
-	creator.create("Pair", list, fitness=creator.FitnessMulti)
+	creator.create("Handside", gp.PrimitiveTree, target_func=target_func)
+	creator.create("Individual", list, fitness=creator.FitnessMulti)
 
 	toolbox = base.Toolbox()
 
 	toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=3)
-	toolbox.register("indTree", tools.initIterate, creator.Tree, toolbox.expr)
-	toolbox.register("pair", tools.initRepeat, creator.Pair, toolbox.indTree, 2)
-	#toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-	toolbox.register("population", tools.initRepeat, list, toolbox.pair)
+	toolbox.register("handside", tools.initIterate, creator.Handside, toolbox.expr)
+	toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.handside, 2)
+	toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 	toolbox.register("compile", gp.compile, pset=pset)
 
 	eval_range = [x/20. * math.pi for x in range(-20,20)] + [x for x in range(-20, 20)]
 	toolbox.register("evaluate", get_fitness, target_func=target_func, toolbox=toolbox, points=eval_range)
 
-	
 	# add filters for unwanted behaviour
 	for filter_ in filters:
 		toolbox.decorate('evaluate', tools.DeltaPenalty(filter_, [1000., 0.]))
 	
 	toolbox.register("select", tools.selSPEA2)
-	#toolbox.register("mate", gp.cxOnePoint)
-	toolbox.register("mate", extendedCxOnePoint)
+	toolbox.register("mate", twosided_mate)
 	toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-	#toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
-	toolbox.register("mutate", extendedMutUniform, expr=toolbox.expr_mut, pset=pset)
+	toolbox.register("mutate", twosided_mutate, expr=toolbox.expr_mut, pset=pset)
 
-	#toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-	toolbox.decorate("mate", extendedSetStaticLimit(key=operator.attrgetter("height"), max_value=17))
-	#toolbox.decorate("mate", gp.staticLimit(key=len, max_value=20))
-	toolbox.decorate("mate", extendedSetStaticLimit(key=len, max_value=20))
-	#toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-	toolbox.decorate("mutate", extendedSetStaticLimit(key=operator.attrgetter("height"), max_value=17))
-	#toolbox.decorate("mutate", gp.staticLimit(key=len, max_value=20))
-	toolbox.decorate("mutate", extendedSetStaticLimit(key=len, max_value=20))
+	toolbox.decorate("mate", gp.staticLimit(key=get_max_height, max_value=17))
+	toolbox.decorate("mate", gp.staticLimit(key=get_max_len, max_value=20))
+	toolbox.decorate("mutate", gp.staticLimit(key=get_max_height, max_value=17))
+	toolbox.decorate("mutate", gp.staticLimit(key=get_max_len, max_value=20))
 	return toolbox
--- helperFunctions.py
+++ helperFunctions.py
@@ -36,38 +36,3 @@ def protectedPow(left, right):
 
 	except OverflowError:
 		return sys.float_info.max
-
-
-def extendedCxOnePoint(ind1, ind2):
-	lhsInd1, rhsInd1 = ind1[0], ind1[1]
-	lhsInd2, rhsInd2 = ind2[0], ind2[1]
-	# In current state, simply crossover lhs with lhs and rhs with rhs
-	ind1[0], ind1[1] = gp.cxOnePoint(lhsInd1, lhsInd2)
-	ind2[0], ind2[1] = gp.cxOnePoint(rhsInd1, rhsInd2)
-
-	return ind1, ind2
-
-def extendedMutUniform(ind1, expr, pset):
-	lhsInd1, rhsInd1 = ind1[0], ind1[1]
-	ind1[0], = gp.mutUniform(lhsInd1, expr, pset)
-
-	if random.uniform(0,1.0) > 0.7 :
-		ind1[1], = gp.mutUniform(rhsInd1, expr, pset)
-
-	# return tuple following the deap mutation function
-	return ind1,
-
-def extendedSetStaticLimit(key, max_value):
-	
-	def decorator(func):
-		@wraps(func)
-		def wrapper(*args, **kwargs):
-			keep_inds = [copy.deepcopy(ind) for ind in args]
-			new_inds = list(func(*args, **kwargs))
-			for i, ind in enumerate(new_inds):
-				if key(ind[0]) > max_value or (len(ind) > 1 and key(ind[1]) > max_value):
-					new_inds[i] = random.choice(keep_inds)
-			return new_inds
-		return wrapper
-
-	return decorator
